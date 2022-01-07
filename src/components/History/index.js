50e199947e246151450c9fdeaaab53670630c537
rodnei0 <rodnei0@gmail.com>
2022-01-06 20:33:54 -0400

    tela historico

--- src/components/History/index.js
+++ src/components/History/index.js
@@ -1,6 +1,15 @@
+import Bottom from '../BottomBar';
+import Top from '../TopBar';
+import { Container} from './styles';
+
 function History() {
     return (
-        <h1>TESTE HISTORICO</h1>
+        <Container>
+            <Top />
+            <h1>Histórico</h1>
+            <h3>Em breve você poderá ver o histórico dos seus hábitos aqui!</h3>
+            <Bottom />
+        </Container>
     );
 };
 
